a43adc2540a5b3b4897db79576c807b284e81318
caozheng1127 <caozheng1127@gmail.com>
2018-06-21 14:54:39 +0800

    update

---
+++ sword/src/数组中出现次数超过一半的数字.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class 数组中出现次数超过一半的数字 {
+    public int MoreThanHalfNum_Solution(int [] array) {
+        int len=array.length;
+        if(len<1){
+            return 0;
+        }
+        int count=0;
+        Arrays.sort(array);
+        int num=array[len/2];
+        for(int i=0;i<len;i++){
+            if(num==array[i])
+                count++;
+        }
+        if(count<=(len/2)){
+            num=0;
+        }
+        return num;
+    }
+}
---
+++ sword/src/最小的K个数.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+//用最大堆保存这k个数，每次只和堆顶比，如果比堆顶小，删除堆顶，新数入堆。
+public class 最小的K个数 {
+    public ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        int length = input.length;
+        if(k > length || k == 0){
+            return result;
+        }
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(k, new Comparator<Integer>() {
+
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+        for (int i = 0; i < length; i++) {
+            if (maxHeap.size() != k) {
+                maxHeap.offer(input[i]);
+            } else if (maxHeap.peek() > input[i]) {
+                Integer temp = maxHeap.poll();
+                temp = null;
+                maxHeap.offer(input[i]);
+            }
+        }
+        for (Integer integer : maxHeap) {
+            result.add(integer);
+        }
+        return result;
+    }
+}
