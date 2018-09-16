4091ff1dfa02e8940e2bf5d4e92adddbb1bdb6a7
caozheng <caozheng1127@gmail.com>
2018-09-16 09:38:45 +0800

    update

--- Main/src/histoty/Main28.java
+++ Main/src/histoty/Main28.java
@@ -1,12 +1,14 @@
+package histoty;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
  * @Description
- * @date 2018/8/11 14:48
+ * @date 2018/8/11 14:49
  */
-public class Main26 {
+public class Main28 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String input = null;
